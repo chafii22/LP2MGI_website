f7c134b6f9bec829dffd6725a96fe8d7b6ee0103
chafii22 <fchafii05@gmail.com>
2026-03-09 20:52:21 +0000

    Initial commit

---
+++ frontend/app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-semibold">
+        LP2MGI Laboratory Website
+      </h1>
+    </main>
+  );
+}
